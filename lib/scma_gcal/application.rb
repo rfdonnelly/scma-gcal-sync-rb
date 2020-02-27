72352b03eeefe366cd307c9809177062f01df457
Rob Donnelly <rfdonnelly@gmail.com>
2020-02-27 14:39:13 -0800

    refactor: move main into Application class

---
+++ lib/scma_gcal/application.rb
@@ -0,0 +1,28 @@
+module SCMAGCal
+  class Application
+    def run(argv)
+      begin
+        run_with_exceptions(argv)
+      rescue SCMAGCal::Error => e
+        $stderr.puts e.message
+        exit 1
+      end
+    end
+
+    def run_with_exceptions(argv)
+      options = SCMAGCal::OptionParser.new.parse(argv.clone)
+
+      input =
+        case options.input
+        when :web
+          Web.new(options.username, options.password)
+        when :yaml
+          SCMAGCal::Input::YAML.new(options.file)
+        end
+
+      events = input.read
+
+      options.output.write(events)
+    end
+  end
+end
