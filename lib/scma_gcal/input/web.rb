a2012e56a999d30cf652951cafd4d40cfc073ee2
Rob Donnelly <rfdonnelly@gmail.com>
2020-02-28 23:51:53 -0800

    refactor: web input to prep for detail page

--- lib/scma_gcal/input/web.rb
+++ lib/scma_gcal/input/web.rb
@@ -1,6 +1,53 @@
 module SCMAGCal
   module Input
     class Web
+      class EventsPage
+        def remote_page(agent)
+          agent.get("https://www.rockclimbing.org/index.php/event-list/events-list")
+        end
+
+        def local_page(path)
+          Nokogiri::HTML(File.open(path))
+        end
+
+        def parse(page)
+          extract_event_lines(page)
+            .map { |line| make_event(parse_event(line)) }
+        end
+
+        def extract_event_lines(page)
+          page.search("tr").map { |row| row.text.remove_nbsp.collapse_whitespace.strip }
+            .chunk { |line| !line.empty? || nil }
+            .map { |_, event_lines| event_lines.join(" ") }
+        end
+
+        def parse_event(event_line)
+          event_line.match(/^(?<start_date>.+?) - (?<end_date>.+?) (?<subject>.+) @ (?<location>.+)/).to_hash
+        end
+
+        def make_event(event_hash)
+          event = SCMAGCal::Model::Event.new(event_hash)
+
+          event.subject = "SCMA: " + event.subject.strip.sub(/,$/, "")
+          event.location = event.location.strip
+          event.start_date = date_from_s(event.start_date)
+          event.end_date = date_from_s(event.end_date)
+
+          # WORKAROUND bug(?) in Google Calendar
+          # All day events spanning multiple days show as 1 day short.
+          # Add one day to end date to compensate.
+          if event.end_date != event.start_date
+            event.end_date += 1
+          end
+
+          event
+        end
+
+        def date_from_s(s)
+          Date.strptime(s, "%m/%d/%y")
+        end
+      end
+
       attr_reader :username
       attr_reader :password
 
@@ -24,56 +71,12 @@ module SCMAGCal
         # FIXME add check for invalid login
       end
 
-      def get_events_page_remote(username, password)
-        agent = make_agent()
-        login(agent, username, password)
-        agent.get("https://www.rockclimbing.org/index.php/event-list/events-list")
-      end
-
-      def get_events_page_local()
-        # FIXME make a command line option
-        Nokogiri::HTML(File.open("events.html"))
-      end
-
-      def extract_event_lines(page)
-        page.search("tr").map { |row| row.text.remove_nbsp.collapse_whitespace.strip }
-          .chunk { |line| !line.empty? || nil }
-          .map { |_, event_lines| event_lines.join(" ") }
-      end
-
-      def parse_event(event_line)
-        event_line.match(/^(?<start_date>.+?) - (?<end_date>.+?) (?<subject>.+) @ (?<location>.+)/).to_hash
-      end
-
       def read
-        # FIXME make commandline option to select input source
-        page = get_events_page_remote(username, password)
-        #page = get_events_page_local()
+        agent = make_agent
+        login(agent, username, password)
+        events_page = EventsPage.new
 
-        extract_event_lines(page)
-          .map { |line| make_event(parse_event(line)) }
-      end
-
-      def make_event(event_hash)
-        event = SCMAGCal::Model::Event.new(event_hash)
-
-        event.subject = "SCMA: " + event.subject.strip.sub(/,$/, "")
-        event.location = event.location.strip
-        event.start_date = date_from_s(event.start_date)
-        event.end_date = date_from_s(event.end_date)
-
-        # WORKAROUND bug(?) in Google Calendar
-        # All day events spanning multiple days show as 1 day short.
-        # Add one day to end date to compensate.
-        if event.end_date != event.start_date
-          event.end_date += 1
-        end
-
-        event
-      end
-
-      def date_from_s(s)
-        Date.strptime(s, "%m/%d/%y")
+        events_page.parse(events_page.remote_page(agent))
       end
     end
   end
