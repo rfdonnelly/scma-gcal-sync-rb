b56ef030589c5ee3aa4cc36f20decb05145150c4
Rob Donnelly <rfdonnelly@gmail.com>
2020-03-02 14:59:53 -0800

    refactor: make more diff friendly

--- lib/scma_gcal/model/event.rb
+++ lib/scma_gcal/model/event.rb
@@ -1,7 +1,12 @@
 module SCMAGCal
   module Model
     class Event
-      attr_accessor :subject, :start_date, :end_date, :location, :url, :description
+      attr_accessor :subject
+      attr_accessor :start_date
+      attr_accessor :end_date
+      attr_accessor :location
+      attr_accessor :url
+      attr_accessor :description
 
       def initialize(event_hash)
         event_hash.each do |k, v|
